f8bf2d0ff74f2e39950e5b1e4699aa8d8a106044
Admin <Admin@DESKTOP-RVF442I>
2022-05-10 13:15:40 +0530

    First commit

---
+++ Day2_JAVA/src/Example1.java
@@ -0,0 +1,19 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int a=15;
+		float b=10.14f;
+		double c=15.1;
+		char d='b';
+		String str="Yatish";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(str);
+		// TODO Auto-generated method stub
+
+	}
+
+}
