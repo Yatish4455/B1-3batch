3189449bee3cec57dddf9ed1701324599e1426f3
Admin <Admin@DESKTOP-RVF442I>
2022-05-12 13:00:10 +0530

    First commite

---
+++ Day3_B1-3/src/Example1.java
@@ -0,0 +1,11 @@
+
+public class Example1 {Example1()
+	{
+	System.out.println("Welcome");
+}
+public static void main(String[] args) {
+	Example1 e=new Example1();
+
+}
+
+}
---
+++ Day3_B1-3/src/car.java
@@ -0,0 +1,17 @@
+
+public class car {
+	String name="BMW";
+void print()
+{
+	System.out.println("This is"+name);
+}
+
+public static void main(String[] args) {
+	
+   car obj1=new car();
+  
+   obj1.print();
+ 
+}
+
+}
